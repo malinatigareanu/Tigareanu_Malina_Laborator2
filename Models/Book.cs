83a103877d6a0717e1ca382aef214c94243e6a80
malinatigareanu <malina.tigareanu@stud.ubbcluj.ro>
2022-11-15 18:36:44 +0200

    Laborator6

--- Models/Book.cs
+++ Models/Book.cs
@@ -11,8 +11,9 @@ namespace Tigareanu_Malina_Laborator2.Models
         public int ID { get; set; }
         [Display(Name = "Book Title")]
         public string Title { get; set; }
-        
-        [Column(TypeName = "decimal(6, 2)")]
+
+        [Column(TypeName = "decimal(6, 2)")
+        [Range(0.01, 500)]
         public decimal Price { get; set; }
 
         [DataType(DataType.Date)]
--- Program.cs
+++ Program.cs
@@ -3,15 +3,27 @@ using Microsoft.Extensions.DependencyInjection;
 using Tigareanu_Malina_Laborator2.Data;
 using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminPolicy", policy =>
+    policy.RequireRole("Admin"));
+});
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Books");
+    options.Conventions.AuthorizeFolder("/Books/Index");
+    options.Conventions.AllowAnonymousToPage("/Books/Details");
+    options.Conventions.AuthorizeFolder("/Members", "AdminPolicy");
+});
 builder.Services.AddDbContext<Tigareanu_Malina_Laborator2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Tigareanu_Malina_Laborator2Context") ?? throw new InvalidOperationException("Connection string 'Tigareanu_Malina_Laborator2Context' not found.")));
 
 builder.Services.AddDbContext<LibraryIdentityContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("Tigareanu_Malina_Laborator2Context") ?? throw new InvalidOperationException("Connection string 'Nume_Pren_Lab2Context' not found.")));
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
 .AddEntityFrameworkStores<LibraryIdentityContext>();
 var app = builder.Build();
 
